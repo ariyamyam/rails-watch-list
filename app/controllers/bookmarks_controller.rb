55ed909aa11185c457973236ba250247e9851d08
Sophie Hadeln <sophiehadeln@gmail.com>
2022-02-20 14:11:17 +0100

    trying again

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,4 +1,10 @@
 class BookmarksController < ApplicationController
+  before_action :set_list, only: [:new, :create]
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
   def create
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
@@ -20,11 +26,7 @@ class BookmarksController < ApplicationController
     params.require(:bookmark).permit(:comment, :movie_id)
   end
 
-  # def set_bookmark
-  #   @bookmark = Bookmark.find(params[:id])
-  # end
-
-  # def set_list
-  #   @list = List.find(params[:list_id])
-  # end
+  def set_list
+    @list = List.find(params[:list_id])
+  end
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,11 +1,11 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :destroy]
   def new
     @list = List.new
   end
 
   def create
     @list = List.new(list_params)
-    @list.save
     if @list.save
       redirect_to list_path(@list)
     else
@@ -18,10 +18,19 @@ class ListsController < ApplicationController
   end
 
   def show
-    @list = List.find(params[:id])
+    @bookmark = Bookmark.new
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to list_path
   end
 
   private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
 
   def list_params
     params.require(:list).permit(:name)
